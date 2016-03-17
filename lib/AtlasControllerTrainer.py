9ceea5a381d7af35abf18782da6c7661d0ddbaee
Vadim Lopatkin <vadim.lopatkin@tum.de>
2016-03-17 21:05:06 +0100

    WIP: learning

--- lib/AtlasControllerTrainer.py
+++ lib/AtlasControllerTrainer.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from atlas_msgs.msg import AtlasSimInterfaceCommand, AtlasSimInterfaceState
 import rospy
+import numpy as np
 
 from lib.AtlasJointsInfo import AtlasJointsInfo
 
@@ -16,21 +17,42 @@ class AtlasControllerTrainer:
         self._calls_counter = 0
 
     @classmethod
-    def train_the_network(cls,controller):
+    def train_the_network(cls, controller):
         global INSTANCE
         INSTANCE.increase_calls_counter()
         if INSTANCE.get_current_behavior() == AtlasSimInterfaceCommand.WALK:
-            print str(INSTANCE.get_trainings_counter())+": network is being " \
-                                                     "trained"
+            print str(INSTANCE.get_trainings_counter()) + ": network is being " \
+                                                          "" \
+                                                          "trained"
             controller_output = controller.get_output()
             print "validating controller output"
             cls._validate_output(controller_output)
             expected_output = controller.get_current_state().position
             print "validating expected output"
             cls._validate_output(expected_output)
+            absolute_deltas = cls._compute_absolute_deltas(controller_output,
+                                                           expected_output)
+            relative_deltas = cls._compute_relative_deltas(absolute_deltas,
+                                                           expected_output)
+            root_mean_square_error = cls._rmse(controller_output,
+                                               expected_output)
+            print "Max absolute error: " + str(
+                np.amax(absolute_deltas)) + "\nMax relative " \
+                                            "error: " + str(
+                np.amax(relative_deltas)) + ".\nRMSE: " + str(
+                    root_mean_square_error)
+            # TODO at this point what we need to do is to backpropagate the
+            # error per neuron. Each absolute error value should be converted
+            #  into Hz (frequency) values, for each neuron in output layer
+            # should be identified the neurons that fired at the same time (
+            # more or less - tbd) as the output layer neuron. Afterwards,
+            # every synapse, that was used for simultaneously fired
+            # neurons, should be adjusted accordingly (not clear how to
+            # measure to which value the adjustment should happen).
             INSTANCE.increase_trainings_counter()
         else:
-            print str(INSTANCE.get_calls_counter())+": network will not be trained"
+            print str(
+                INSTANCE.get_calls_counter()) + ": network will not be trained"
 
     def get_current_behavior(self):
         return self._current_behavior
@@ -71,6 +93,30 @@ class AtlasControllerTrainer:
                                                            "minimum allowed " \
                                                            "value by " + str(
                 ratio)
+
+    @classmethod
+    def _compute_absolute_deltas(cls, controller_output, expected_output):
+        expected_output_list = list(expected_output)
+        result = []
+        for i in xrange(len(controller_output)):
+            delta = abs(expected_output_list[i] - controller_output[i])
+            result.append(delta)
+        return result
+
+    @classmethod
+    def _compute_relative_deltas(cls, absolute_deltas, expected_output):
+        expected_output_list = list(expected_output)
+        result = []
+        for i in xrange(len(expected_output_list)):
+            delta = absolute_deltas[i] / expected_output_list[i]
+            result.append(delta)
+        return result
+
+    @classmethod
+    def _rmse(cls, controller_output, expected_output):
+        expected_output_list = list(expected_output)
+        return np.sqrt(((np.asarray(expected_output_list) -
+                         np.asarray(controller_output)) ** 2).mean())
 
 
 INSTANCE = AtlasControllerTrainer()
